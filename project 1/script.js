bba5cefa33056b843487836266c87f6f3e33d03f
chaudhary-rajkumar <prorajkumar555@gmail.com>
2025-07-28 10:28:56 +0545

    completed basic layout and index page

--- project 1/script.js
+++ project 1/script.js
@@ -1,31 +1,34 @@
-const form=document.getElementById("myform");
-        const username=document.getElementById("username"); 
-        const userage=document.getElementById("age");
-        const nameError=document.getElementById("nameError");
-        const ageError=document.getElementById("ageError");
-        const successMessage=document.getElementById("successMessage");
 
-        form.addEventListener("submit",function(e){
-            e.preventDefault();
+const form = document.getElementById("myform");
 
-            let valid=true;
-            nameError.textContent = "";
-            ageError.textContent = "";
-            successMessage.textContent = "";
+const username = document.getElementById("username");
 
-            if(username.value.trim().length < 3){
-                nameError.textContent = "Name must be at least 3 characters long";
-                valid = false;
-            }
-            if(userage.value < 18){
-                ageError.textContent = "You must be at least 18 years old";
-                valid = false;
-            }
-            if(valid){
-                successMessage.textContent="Form Submitted successfully!";
-                form.reset();
-            }
+const userage = document.getElementById("age");
 
+const nameError = document.getElementById("nameError");
 
+const ageError = document.getElementById("ageError");
 
-        });
+const successMessage = document.getElementById("successMessage");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let valid = true;
+  nameError.textContent = "";
+  ageError.textContent = "";
+  successMessage.textContent = "";
+
+  if (username.value.trim().length < 3) {
+    nameError.textContent = "Name must be at least 3 characters long";
+    valid = false;
+  }
+  if (userage.value < 18) {
+    ageError.textContent = "You must be at least 18 years old";
+    valid = false;
+  }
+  if (valid) {
+    successMessage.textContent = "Form Submitted successfully!";
+    form.reset();
+  }
+});
